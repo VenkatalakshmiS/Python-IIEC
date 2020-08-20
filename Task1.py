de318a18b0b07c2b9282db23d02f2b4b90df8b81
VENKATALAKSHMI SUBRAMANIAM <68234899+VenkatalakshmiS@users.noreply.github.com>
2020-08-20 17:59:56 +0530

    IIEC Task 1

---
+++ Task1.py
@@ -0,0 +1,32 @@
+import os
+import tkinter as tk  
+from tkinter import ttk
+  
+win = tk.Tk()# Application Name  
+win.title("Python Task : 1")# Label  
+lbl = ttk.Label(win, text = "Enter aplication name u want to open:").grid(column = 0, row = 0)# Click event
+
+def click():   
+  while True:
+    userinput = name.get()
+    # os.system(userinput)
+    if("run" in userinput) or ("google" in userinput) and ("chrome" in userinput):
+      os.system("chrome")
+    elif(("run" in userinput)or("open" in userinput)or("execute" in userinput)) and ("notepad" in userinput):
+      os.system("notepad")
+    elif(("run" in userinput) or ("open" in userinput) or ("execute" in userinput)) and ("paint in userinput"):
+      os.system("mspaint")
+    elif(("run" in userinput) or ("open" in userinput) or ("execute" in userinput)) and ("wordpad in userinput"):
+      os.system("wordpad")
+    elif(("run" in userinput) or ("open" in userinput) or ("execute" in userinput)) and ("media" in userinput) and ("player" in userinput):
+      os.system("wmplayer")
+    elif("exit" in userinput) or ("quit" in userinput):
+      break
+  else:
+    print("Not valid")
+  
+
+name = tk.StringVar()  
+nameEntered = ttk.Entry(win, width = 22, textvariable = name).grid(column = 0, row = 1)# Button widget  
+button = ttk.Button(win, text = "submit", command = click).grid(column = 1, row = 1)  
+win.mainloop()   
